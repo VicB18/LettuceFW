71ace8fa3aebad44392b607e99d69f5886b0f1cb
Victor Bloch <47314151+VicB18@users.noreply.github.com>
2023-11-12 18:59:39 +0200

    Delete garesults.m

--- garesults.m
+++
@@ -1,9 +0,0 @@
-function garesults(gaDat)
-% Optional user task executed when the algorithm ends
-
-% For instance, final result
-disp('------------------------------------------------')
-disp('######   RESULT   #########')
-disp(['   Objective function for xmin: ' num2str(gaDat.fxmin,5)])
-disp(['   xmin: ' mat2str(gaDat.xmin,3)])
-disp('------------------------------------------------')
