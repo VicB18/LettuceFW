9873ab7f1c497fccc9349e0b55f829369fb6ab43
Victor Bloch <47314151+VicB18@users.noreply.github.com>
2023-11-12 18:59:56 +0200

    Delete gaiteration.m

--- gaiteration.m
+++
@@ -1,4 +0,0 @@
-function gaiteration(gaDat)
- disp('------------------------------------------------')
- disp(['Iteration: ' num2str(gaDat.gen)])
- disp(['   xmin: ' mat2str(gaDat.xmin,3) ' -- f(xmin): ',num2str(gaDat.fxmin,5)])
